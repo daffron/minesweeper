827139c710012eb72a81f72b3def67618a6491a7
daffron <daffron92@gmail.com>
2017-03-28 17:56:22 +1300

    stretch goal 2

--- minesweeper.js
+++ minesweeper.js
@@ -2,25 +2,26 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board ={
-  cells: [
-  {row: 0, col: 0, isMine: true, hidden:true},
-  {row: 0, col: 1, isMine: false, hidden:true},
-  {row: 0, col: 2, isMine: false, hidden:true},
-  {row: 1, col: 0, isMine: false, hidden:true},
-  {row: 1, col: 1, isMine: false, hidden:true},
-  {row: 1, col: 2, isMine: false, hidden:true},
-  {row: 2, col: 0, isMine: true, hidden:true},
-  {row: 2, col: 1, isMine: false, hidden:true},
-  {row: 2, col: 2, isMine: false, hidden:true}]
+  cells: []
 };
 
+
+
 function startGame () {
+  createBoard(prompt("how many rows?"))
+
   for (var i = 0; i < board.cells.length; i++){
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
   lib.initBoard()
-  document.addEventListener('click', checkForWin)
-  document.addEventListener('contextmenu', checkForWin)
+  document.getElementById('gameboard').addEventListener('click', function(){
+  checkForWin();
+  checkLose();
+})
+  document.getElementById('gameboard').addEventListener('contextMenu', function(){
+checkForWin();
+checkLose();
+})
 }
 
 // Define this function to look for a win condition:
@@ -28,21 +29,26 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
   for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine && !board.cells[i].isMarked) {
+      return
+    } else if (board.cells[i].hidden && !board.cells[i].isMine) {
+      return
+    }
+  }
+  // You can use this function call to declare a winner (once you've
+  // detected that they've won, that is!)
+  //   lib.displayMessage('You win!')
+  lib.displayMessage('You win!')
+  setTimeout(function() {restart(); }, 2500);
 
-    if (board.cells[i].isMine && board.cells[i].isMarked){
-      if (!board.cells[0,1,2,3,4,5,6,7,8].hidden){
-     lib.displayMessage('You win!')
-   }
-   }
-
-
-
-
+}
+function checkLose () {
+  for (var i = 0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine && !board.cells[i].hidden)
+    setTimeout(function() {restart(); }, 2500);
   }
 }
-
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
 
@@ -64,4 +70,56 @@ function countSurroundingMines (cell) {
     }
   }
   return count;
+}
+
+function createBoard (x){
+
+
+  for (var i = 0; i < x*x; i++){
+    board.cells[i] = {
+      row: Math.floor(i/x),
+      col: i %  x,
+      isMine: Math.random() < 0.25 ? true : false,
+      isMarked: false,
+      hidden: true
+    }
+  }
+
+}
+
+
+
+
+
+function restart() {
+  var one = document.getElementById('gameboard')
+  one.classList.add('hide');
+  var two = document.getElementById('restart')
+  two.classList.remove('hide')
+}
+window.onload=function(){
+
+document.getElementById('play').addEventListener('click',function(){
+  Play()
+})
+document.getElementById('noplay').addEventListener('click',function(){
+  noPlay()
+})
+}
+function Play (){
+    window.location.reload();
+    console.log("test")
+
+}
+function play (){
+  var y = document.getElementById('restart')
+  y.classList.add('hide')
+  var x = document.getElementById('gameboard')
+  x.classList.remove('hide')
+}
+function noPlay(){
+  var y = document.getElementById('restart')
+  y.classList.add('hide')
+  var x = document.getElementById('gameboard')
+  x.classList.remove('hide')
 }
